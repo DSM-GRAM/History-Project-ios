e80c8d1e118cef486f05e04dc43375eb84a2df20
DAEUN28 <acone1128@gmail.com>
2019-04-26 23:47:58 +0900

    first commit

---
+++ History-Project-ios/History-Project-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  History-Project-ios
+//
+//  Created by baby1234 on 26/04/2019.
+//  Copyright © 2019 baby1234. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # History-Project-ios
+
+## 역사 프로젝트(독립운동 바로 알기 - 연해주편)
+
+기간 : 5/31까지
+
+- 지도 -> 정
+- 유적지 목록 ->정
+- 유적지 정보 -> 김
+- 맵 + VR이미지 -> 김
+- 테스트 -> 정
+
+사용 라이브러리
+- RxAlmofire
+- Codable
+- ObjectMapper
+- Kingfisher
+- Romation
