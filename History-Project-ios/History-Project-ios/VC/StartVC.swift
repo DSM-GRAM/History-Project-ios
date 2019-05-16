fca7d378fa387ea63059d396a4cfc31853b28d53
Jeongdasilver <alal8272@gmail.com>
2019-05-16 15:53:31 +0900

    UPDATE VC, UI

--- History-Project-ios/History-Project-ios/VC/StartVC.swift
+++ History-Project-ios/History-Project-ios/VC/StartVC.swift
@@ -1,20 +1,17 @@
 //
-//  ViewController.swift
+//  StartVC.swift
 //  History-Project-ios
 //
-//  Created by baby1234 on 26/04/2019.
+//  Created by daeun on 16/05/2019.
 //  Copyright © 2019 baby1234. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class StartVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
-
