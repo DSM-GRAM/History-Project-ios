29e2af235b6e1b932e795dc9076aa2649b5087d6
Jeongdasilver <alal8272@gmail.com>
2019-05-23 23:16:36 +0900

    ADD StartVC, StartViewModel, Shape, ViewModelType

--- History-Project-ios/History-Project-ios/VC/StartVC.swift
+++ History-Project-ios/History-Project-ios/VC/StartVC.swift
@@ -7,11 +7,33 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class StartVC: UIViewController {
+    
+    @IBOutlet weak var blaButton: RoundButton!
+    @IBOutlet weak var usuButton: RoundButton!
+    
+    let disposeBag = DisposeBag()
+    var startViewModel: StartViewModel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        startViewModel = StartViewModel()
+        let input = StartViewModel.Input(clickUsu: blaButton.rx.tap.asSignal(),
+                                         clickBla: usuButton.rx.tap.asSignal())
+        let output = startViewModel.transform(input: input)
+        
+        output.area.asObservable().subscribe { [weak self] _ in
+            guard let strongSelf = self else {return}
+            strongSelf.performSegue(withIdentifier: "goHistoricalSiteList", sender: nil)
+        }.disposed(by: disposeBag)
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = true
     }
 }
