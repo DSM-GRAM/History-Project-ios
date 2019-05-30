9e86a4eb5d3342d50d7a59de002ae6701b399511
Jeongdasilver <alal8272@gmail.com>
2019-05-30 17:12:14 +0900

    UPDATE StartVC perform segue

--- History-Project-ios/History-Project-ios/VC/StartVC.swift
+++ History-Project-ios/History-Project-ios/VC/StartVC.swift
@@ -17,20 +17,28 @@ class StartVC: UIViewController {
     
     let disposeBag = DisposeBag()
     var startViewModel: StartViewModel!
-
+    var area = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         startViewModel = StartViewModel()
-        let input = StartViewModel.Input(clickUsu: blaButton.rx.tap.asSignal(),
-                                         clickBla: usuButton.rx.tap.asSignal())
+        let input = StartViewModel.Input(clickUsu: usuButton.rx.tap.asSignal(),
+                                         clickBla: blaButton.rx.tap.asSignal())
         let output = startViewModel.transform(input: input)
         
-        output.area.asObservable().subscribe { [weak self] _ in
+        output.area.asObservable().subscribe { [weak self] area in
             guard let strongSelf = self else {return}
+            strongSelf.area = area.element ?? ""
             strongSelf.performSegue(withIdentifier: "goHistoricalSiteList", sender: nil)
-        }.disposed(by: disposeBag)
-        
+            }.disposed(by: disposeBag)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goHistoricalSiteList" {
+            let vc = segue.destination as? HistoricalSiteListVC
+            vc?.area = area
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
